c1ab301210c96b44f2a1b133d5c0fdd0c12e3546
Сергей <samajor@mail.ru>
2021-04-14 12:34:15 +0300

    2. Список адресов [#235392]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ProfilesTest {
+
+    @Test
+    public void whenGetAddress() {
+        List<Profile> listProfiles = List.of(
+                new Profile(new Address("Bryansk", "Pushkina", 10, 77)),
+                new Profile(new Address("Moskva", "Lenina", 12, 4)));
+        Profiles profiles = new Profiles();
+        List<Address> rsl = profiles.collect(listProfiles);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Bryansk", "Pushkina", 10, 77));
+        expected.add(new Address("Moskva", "Lenina", 12, 4));
+        assertThat(rsl, is(expected));
+    }
+}
